265d69b57bb1b8ebcb12ebad15e5c56800ce0294
Nathan Glenn <garfieldnate@gmail.com>
2021-05-07 21:05:07 +0200

    Project 2: graphics working

--- cs168/mini_project_2.py
+++ cs168/mini_project_2.py
@@ -12,11 +12,17 @@ def load_data():
         zip_ref.extractall(ZIP_FILE.parents[0])
 
     group_names = pd.read_csv(DATA_DIR / "groups.csv", header=None, squeeze=True)
+    labels = pd.read_csv(DATA_DIR / "label.csv", header=None, squeeze=True)
+
+    # data uses 1-indexing, so update indexes to match
+    group_names.index += 1
+    labels.index += 1
+
     data = pd.read_csv(
         DATA_DIR / "data50.csv",
         header=None,
         names=["article_id", "word_id", "count"],
     ).pivot(index="article_id", columns="word_id")["count"]
-    labels = pd.read_csv(DATA_DIR / "label.csv", header=None, squeeze=True)
+    data.fillna(0, inplace=True)
 
     return group_names, data, labels
